10fbb4590f3845e4fb07dd7ae947ab5adf16bb5d
Cbaerts <cbaerts@uwo.ca>
2024-12-02 16:09:15 -0500

    Update BaertsCedric_lab12.py
    I obtained the data for part 1
    Infuriating as F***

--- BaertsCedric_lab12.py
+++ BaertsCedric_lab12.py
@@ -1,3 +1,10 @@
 import numpy as np
 
-print("Im dazing in liams eyes")
+file = '/Users/cedricbaerts/Desktop/Physics3926/Week12/Lab12/co2_mm_mlo.csv'
+
+csvFile = np.loadtxt(file, skiprows=52, delimiter=",")
+
+condition = ((1981 <= csvFile[:, 0]) & (csvFile[:, 0] <= 1990))
+index = np.where(condition)
+part1 = csvFile[index]
+print(part1)
