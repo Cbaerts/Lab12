a0d960db381bbc1b2815e2d5ebae3530129738ed
Cbaerts <cbaerts@uwo.ca>
2024-12-04 17:43:47 -0500

    Update BaertsCedric_lab12.py
    Added Part3

--- BaertsCedric_lab12.py
+++ BaertsCedric_lab12.py
@@ -13,6 +13,9 @@ part1 = csvFile[index]
 
 xData = part1[:,2]
 yData = part1[:,3]
+
+# Followed this tutorial to find the polynomial coefficients
+# https://www.youtube.com/watch?v=dQw4w9WgXcQ
 # otherpoly = np.polynomial.polynomial.Polynomial(xData, yData, 4)
 poly = Polynomial.fit(xData, yData, 2, domain=[xData[0], xData[-1]])
 # coefs = poly.convert(domain=poly.domain).coef
@@ -25,12 +28,12 @@ subtracted = yData - yFit
 
 ax[0].plot(xData, yData, label='Data', color='maroon')
 ax[0].plot(xData, yFit, label="Fitted Polynomial", linestyle='--')
-ax[0].set_title("CO2 Data From Mauna Loa, Hawai'i From 1")
+ax[0].set_title("CO2 Data From Mauna Loa, Hawai'i From 1981-1990")
 ax[0].set_ylabel("CO2 Concentration (PPM)")
 ax[0].set_xlabel('Year')
 ax[0].legend()
 ax[1].plot(xData, subtracted, label='Data', color='maroon')
-ax[1].set_title("Subtracted Data")
+ax[1].set_title("Residual Data")
 ax[1].set_ylabel("CO2 Concentration (PPM)")
 ax[1].set_xlabel('Year')
 ax[1].legend()
@@ -39,16 +42,50 @@ plt.savefig('BaertsCedric_Lab12_Fig1')
 plt.close()
 
 # Part 2
-amp = 3.3
+amp = 3.5
 period = 1
-phase = -0.5
-trialNerror = amp* np.sin(2*np.pi*(xData/period)+phase)
+phase = 5.8
+trialNerror = amp* np.sin(2*np.pi*(xData/period) + phase) - 0.3
 
 plt.subplot()
-plt.plot(xData, subtracted, label='Data', color='maroon')
-plt.plot(xData, trialNerror, label='simple sinusoidal function', color='navy')
+plt.plot(xData, subtracted, label='Residual Data', color='maroon')
+plt.plot(xData, trialNerror, label='Simple Sinusoidal Function', color='navy')
 plt.title("Subtracted Data")
+plt.ylabel("Residual CO2 Concentration (PPM)")
+plt.xlabel('Year')
+plt.legend()
+# plt.show()
+plt.close()
+
+fourier = np.fft.fft(subtracted)
+n = subtracted.size
+timestep = xData[1] - xData[0]
+freq = np.fft.fftfreq(n, d=timestep)
+
+plt.plot(freq[:len(freq)//2], np.abs(fourier)[:len(freq)//2], label='All Fourier Amplitude', color='dodgerblue')
+plt.title("Fourier Transform of Residuak Data")
+plt.xlabel("Frequency (1/year)")
+plt.ylabel("Fourier Amplitude")
+plt.axis((0, 6, 0, 200))
+# plt.show()
+plt.close()
+# These values do agree with my estimated period
+
+# Part 3
+condition = ((1981 <= csvFile[:, 0]))
+index = np.where(condition)
+part3 = csvFile[index]
+xData = part3[:,2]
+yData = part3[:,3]
+yFit = poly(xData)
+
+
+plt.plot(xData, yData, label='Data', color='maroon')
+plt.plot(xData, yFit, label="Fitted Polynomial", linestyle='--')
+plt.title("CO2 Data From Mauna Loa, Hawai'i From 1981-2020")
 plt.ylabel("CO2 Concentration (PPM)")
 plt.xlabel('Year')
 plt.legend()
+plt.savefig('BaertsCedric_Lab12_Fig2')
 plt.show()
+plt.close()
